fd99c9e816cc60fdf23198545e76103cda24470d
lcdsr <lcds.rafael@gmail.com>
2018-06-07 12:58:44 +0200

    prep exo position

--- src/app/features/tab-one/containers/tab-one/tab-one.component.ts
+++ src/app/features/tab-one/containers/tab-one/tab-one.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins, GeolocationPosition, Capacitor } from '@capacitor/core';
+import { Observable, of, from as fromPromise} from 'rxjs';
 
 @Component({
   selector: 'app-tab-one',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabOneComponent implements OnInit {
 
+  public coordinates$: Observable<GeolocationPosition>;
   constructor() { }
 
   ngOnInit() {
+    this.getCurrentPosition();
+    
+    this.watchPosition();
   }
 
+  async getCurrentPosition(): Promise<Observable<GeolocationPosition|Error>> {
+
+    const isAvailable: boolean = Capacitor.isPluginAvailable('Geolocation');
+    if (!isAvailable) {
+      console.log('Err: plugin not available');
+      return of(new Error('Err: plugin not available'));
+    }
+    
+    console.log(Plugins.Geolocation.getCurrentPosition());
+    
+    return this.coordinates$ = fromPromise(Plugins.Geolocation.getCurrentPosition());
+  }
+
+  watchPosition() {
+    const wait = Plugins.Geolocation.watchPosition({}, (position, err) => {
+    })
+  }
 }
